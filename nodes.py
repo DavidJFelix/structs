1073e299cca86004ba6eda3e4f9be3695d5aaa78
David J Felix <felix.davidj@gmail.com>
2011-03-16 02:34:21 -0400

    Rewrote nodes.py, removed corresponding tests, added license
    nodes.py was unruly and incorrect for what was needed from a node,
    it also needed new types of nodes. Node was changed from an old style
    object to a new style object, keeping things "pythonic".
    
    test_nodes.py will still need to be populated with adequate tests, which
    do not exist at this time, but old tests were removed to prevent confusion
    in the future. They were not commented out because they no longer had
    relevance to what nodes.py is.
    
    License text is provided, as encouraged by the FSF.

--- nodes.py
+++ nodes.py
@@ -18,272 +18,187 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
-class Node:
-    """
-    """
-    def __abs__(self):
-        return abs(self.content)
-
-    def __add__(self, other):
-        return self.content + other
-
-    def __and__(self, other):
-        return self.content & other
-
-    def __bool__(self):
-        return bool(self.content)
-
-    def __call__(self, *args):
-        return self.content(*args)
-
-    def __complex__(self):
-        return complex(self.content)
-
-    def __contains__(self, item):
-        return item in self.content
-
-#    def __del__(self):
-#        pass
-#
-#    def __delete__(self, insance):
-#        pass
-#
-#    def __delattr__(self, name):
-#        pass
-#
-#    def __delitem__(self, key):
-#        pass
-#
-#    def __dir__(self):
-#        pass
-#
-
-    def __divmod__(self, other):
-        return divmod(self.content, other)
-
-#    def __enter__(self):
-#        pass
-#
-    def __eq__(self, other):
-        return self.content == other
-
-#    def __exit__(self):
-#        pass
-#
-    def __floordiv__(self, other):
-        return self.content // other
-
-    def __float__(self):
-        return float(self.content)
-
-    def __format__(self):
-        return format(self.content)
-
-    def __ge__(self, other):
-        return self.content >= other
-
-#    def __get__(self, insance, owner):
-#        pass
-#
-#    def __getattr__(self, name):
-#        pass
-#
-#    def __getattribute(self, name):
-#        pass
-#
-    def __getitem__(self, key):
-        return self.content[key]
-
-    def __gt__(self, other):
-        return self.content > other
-
-    def __hash__(self):
-        return hash(self.content)
-
-    def __le__(self, other):
-        return self.content <= other
-
-    def __len__(self):
-        return len(self.content)
-
-    def __lshift__(self, other):
-        return self.content << other
-
-    def __lt__(self, other):
-        return self.content < other
-
-    def __iadd__(self, other):
-        self.content += other
-        return self.content
-
-    def __iand__(self, other):
-        self.content &= other
-        return self.content
-
-    def __ifloordiv__(self, other):
-        self.content //= other
-        return self.content
-
-    def __ilshift__(self, other):
-        self.content <<= other
-        return self.content
-
-    def __imod__(self, other):
-        self.content %= other
-        return self.content
-
-    def __imul__(self, other):
-        self.content *= other
-        return self.content
-
-    def __index__(self):
-        return self.content.__index__()
-
-    def __init__(self, content, parent = None):
-        self.content = content
-        self.parent = parent
-        self.children = []
-
-    def __int__(self):
-        return int(self.content)
-
-    def __invert__(self):
-        return ~self.content
-
-    def __ior__(self, other):
-        self.content |= other
-        return self.content
-
-#    def __instancecheck__(self, instance):
-#        pass
-#
-    def __ipow__(self, other, *modulo):
-        self.content = pow(self.content, other, *modulo)
-        return self.content
-
-    def __irshift__(self, other):
-        self.content >>= other
-        return self.content
-
-    def __isub__(self, other):
-        self.content -= other
-        return self.content
-
-#    def __iter__(self):
-#        pass
-#
-    def __itruediv__(self, other):
-        self.content /= other
-        return self.content
-
-    def __ixor__(self, other):
-        self.content ^= other
-        return self.content
-
-    def __mod__(self, other):
-        return self.content % other
-
-    def __mul__(self, other):
-        return self.content * other
-
-    def __ne__(self, other):
-        return self.content != other
-
-    def __neg__(self):
-        return -self.content
-
-#    def __new__(cls[, ...]):
-#        pass
-#
-    def __or__(self, other):
-        return self.content | other
-
-    def __pos__(self):
-        return +self.content
-
-    def __pow__(self,other, *modulo):
-        return pow(self.content, other, *modulo)
-
-    def __radd__(self, other):
-        return other + self.content
-
-    def __rand__(self, other):
-        return other & self.content
-
-    def __rdivmod__(self, other):
-        return divmod(other, self.content)
-
-    def __repr__(self):
-        return repr(self.content)
-
-    def __reversed__(self):
-        return reversed(self.content)
-
-    def __rfloordiv__(self, other):
-        return other // self.content
-
-    def __rlshift__(self, other):
-        return other << self.content
-
-    def __rmod__(self, other):
-        return other % self.content
-
-    def __rmul__(self, other):
-        return other * self.content
-
-    def __ror__(self, other):
-        return other | self.content
-
-    def __round__(self, *n):
-        return round(self.content, *n)
-
-    def __rpow__(self, other):
-        return other ** self.content
-
-    def __rrshift__(self, other):
-        return other >> self.content
-
-    def __rshift__(self, other):
-        return self.content >>  other
-
-    def __rsub__(self, other):
-        return other - self.content
-
-    def __rtruediv__(self, other):
-        return other / self.content
-
-    def __rxor__(self, other):
-        return other ^ self.content
-
-#    def __set__(self, instance, value):
-#        pass
-#
-#    def __setattr(self, name, value):
-#        pass
-#
-#    def __setitem__(self, key):
-#        pass
-#
-#    def __slots__(self):
-#        pass
-#
-    def __str__(self):
-        return str(self.content)
-
-    def __sub__(self, other):
-        return self.content - other
-
-#    def __subclasscheck__(self, subclass):
-#        pass
-#
-    def __truediv__(self, other):
-        return self.content / other
-
-    def __xor__(self, other):
-        return self.content ^ other
+class Node(object):
+    """A container class which is used for creating complex data structures."""
+
+    def __init__(self, data = None):
+        self._data = data
+
+    @property
+    def data(self):
+        """The data property stores what is contained in the node"""
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        self._data = value
+
+    @data.deleter
+    def data(self):
+        del self._data
+        self._data = None
 
 # New node types should be added here and should probably inherit Node
+class LinkedNode(Node):
+    """A container class with one child"""
 
+    def __init__(self, data = None, next = None):
+        super(Node, self).__init__(self, data)
+        self._next = next
 
-        
+    @property
+    def next(self):
+        """The next property stores the node that is linked"""
+        return self._next
 
+    @next.setter
+    def next(self, value):
+        self._data = value
+
+    @next.deleter
+    def next(self):
+        del self._next
+        self._next = None
+
+class BiLinkedNode(LinkedNode):
+    """A bidirectional container class with one child"""
+
+    def __init__(self, data, next, prev):
+        super(LinkedNode, self).__init__(self, data, next)
+        self._prev = prev
+
+    @property
+    def prev(self):
+        """"""
+        return self._prev
+
+    @prev.setter
+    def prev(self, value):
+        self._prev = value
+
+    @prev.deleter
+    def prev(self):
+        del self._prev
+        self._prev = None
+
+class BinaryNode(Node):
+    """A container class with two children"""
+
+    def __init__(self, data, left = None, right = None):
+        super(Node, self).__init__(self, data):
+        self._left = left
+        self._right = right
+
+    @property
+    def left(self):
+        """"""
+        return self._left
+
+    @left.setter
+    def left(self, value):
+        """"""
+        self._left = value
+
+    @left.deleter
+    def left(self):
+        del self._left
+        self._left = None
+
+    @property
+    def right(self):
+        """"""
+        return self._right
+
+    @right.setter
+    def right(self, value):
+        self._right = value
+
+    @right.deleter
+    def right(self):
+        del self._right
+        self._right = None
+
+class BiBinaryNode(BinaryNode):
+    """A bidirectional container class with two children"""
+
+    def __init__(self, data, left = None, right = None, parent = None):
+        super(BinaryNode, self).__init__(self, data, left, right)
+        self._parent = parent
+
+`   @property
+    def parent(self):
+        """"""
+        return self._parent
+
+    @parent.setter
+    def parent(self, value):
+        self._parent = value
+
+    @parent.deleter
+    def parent(self):
+        del self._parent
+        self._parent = None
+
+class MultiNode(Node):
+    """A container class with any number of children"""
+
+    def __init__(self, data, children = set()):
+        super(Node, self).__init__(self, data)
+        self._children = children
+
+    @property
+    def children(self):
+        """"""
+        return self._children
+
+    @children.setter
+    def children(self, value):
+        self._children = value
+
+    @children.deleter
+    def children(self):
+        del self._children
+        self._children = set()
+
+class BiMultiNode(MultiNode):
+    """A container class with any number of unordered children and parents"""
+
+    def __init__(self, data, children = set(), parents = set()):
+        super(MultiNode, self).__init__(self, data, children)
+        self._parents = parents
+
+    @property
+    def parents(self):
+        """"""
+        return self._parents
+
+    @parents.setter
+    def parents(self, value):
+        self._parents = value
+
+    @parents.deleter
+    def parents(self)
+        del self._parents
+        self._parents = set()
+
+class OrderedMultiNode(MulitNode):
+    """A container class with any number of ordered children"""
+
+    def __init__(self, data, children = [])
+        super(MultiNode, self).__init__(self, data, children)
+
+    @children.deleter
+    def children(self):
+        del self._children
+        self._children = []
+
+class BiOrderedMultiNode(BiMultiNode):
+    """A bidirectional container class with any number of ordered children"""
+
+    def __init__(self, data, children = [], parents = []):
+        super(BiMultiNode, self).__init__(self, data, children, parents)
+
+    @parents.deleter
+    def parents(self):
+        del self._parents
+        self._parents = []
