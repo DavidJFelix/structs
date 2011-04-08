b7451ca32156521745cf3b76dc45f520ac3b7517
David J Felix <felix.davidj@gmail.com>
2011-04-08 12:17:14 -0400

    Adding the header all over to complete the transformation.

--- tests/test_hashes.py
+++ tests/test_hashes.py
@@ -1,1 +1,39 @@
-﻿
+﻿#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+#
+# structs/tests/test_hashes.py
+#
+# Copyright (c) 2011 David J Felix
+#
+# Contributors:
+#                    [Add your name here]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");  you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at:
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+# Alternatively, the contents of this file may be used under the terms of 
+# the LGPLv3+ (the  "Lesser GNU General Public License version 3 or later"),
+# the GPLv3+ (the "GNU General Public License version 3 or later"),
+# the AGPLv3+ (the "Affero GNU General Public License version 3 or later"),
+# the MPLv1.1+ (the "Mozilla Public License version 1.1 or later"), in which
+# case the provisions of LGPLv3+, GPLv3+, AGPLv3+, or MPLv1.1+ are respectively
+# applicable instead of those above. If you wish to allow use of your version of
+# this file only under the terms of the LGPLv3+, GPLv3+, AGPLv3+ or MPLv1.1 and
+# not to allow others to use your version of this file under the Apache License,
+# Version 2.0 or another of the above licenses, indicate your decision by
+# deleting the provisions above and replace them with the notice and other
+# provisions required by the LGPLv3+, GPLv3+, AGPLv3+, or MPLv1.1+ respectively.
+# If you do not delete the provisions above, or modify this notice a recipient
+# may use your version of this file under either the Apache License, Version 
+# 2.0, the LGPLv3+, the GPLv3+, the AGPLv3+ or the MPLv1.1+. A full copy of the
+# oldest allowable version of these licenses is available in the "licenses"
+# directory, which is located in the root directory of this project.
