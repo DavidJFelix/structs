e2e0d75d36e1b003a62dba40da06eb9b6341f37f
David J Felix <felix.davidj@gmail.com>
2011-04-08 11:48:57 -0400

    Beginning to change license terms.

--- nodes.py
+++ nodes.py
@@ -3,20 +3,41 @@
 #
 # structs/nodes.py
 #
-# Copyright (C) 2011  David J Felix
+# Copyright (c) 2011 David J Felix
 #
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Affero General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
+# Contributors:
+#                    [Add your name here]
 #
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
-# details.
+# Licensed under the Apache License, Version 2.0 (the "License");  you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at:
 #
-# You should have received a copy of the GNU Affero General Public License along
-# with this program. If not, see <http://www.gnu.org/licenses/>.
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+# Alternatively, the contents of this file may be used under the terms of 
+# the LGPLv3+ (the  "Lesser GNU General Public License version 3 or later"),
+# the GPLv3+ (the "GNU General Public License version 3 or later"),
+# the AGPLv3+ (the "Affero GNU General Public License version 3 or later"),
+# the MPLv1.1+ (the "Mozilla Public License version 1.1 or later"), in which
+# case the provisions of LGPLv3+, GPLv3+, AGPLv3+, or MPLv1.1+ are respectively
+# applicable instead of those above. If you wish to allow use of your version of
+# this file only under the terms of the LGPLv3+, GPLv3+, AGPLv3+ or MPLv1.1 and
+# not to allow others to use your version of this file under the Apache License,
+# Version 2.0 or another of the above licenses, indicate your decision by
+# deleting the provisions above and replace them with the notice and other
+# provisions required by the LGPLv3+, GPLv3+, AGPLv3+, or MPLv1.1+ respectively.
+# If you do not delete the provisions above, or modify this notice a recipient
+# may use your version of this file under either the Apache License, Version 
+# 2.0, the LGPLv3+, the GPLv3+, the AGPLv3+ or the MPLv1.1+. A full copy of the
+# oldest allowable version of these licenses is available in the "licenses"
+# directory, which is located in the root directory of this project.
+
 
 class Node(object):
     """A container class which is used for creating complex data structures.
