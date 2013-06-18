5cbb86af5a4f0b567c1131247ef8b88884e5a3da
David J. Felix <felix.davidj@gmail.com>
2013-06-18 14:36:43 -0300

    Attempt to make rusty structures

--- Rust/nodes.rs
+++ Rust/nodes.rs
@@ -1,1 +1,21 @@
-#
+#[link(name = "nodes", vers = "1.0")]
+
+struct Node<T> {
+	data: T
+}
+
+enum Link<T> {
+	None,
+	Some(~T)
+}
+
+struct LinkedNode<T> {
+	data: T,
+	mut right: Link<LinkedNode<T>>
+}
+
+struct BinaryNode<T> {
+	data T,
+	mut right: Link<BinaryNode<T>>,
+	mut left: Link<BinaryNode<T>>
+}
